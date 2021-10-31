280e489b10231035fa464b7c52875e4feb92c8f2
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-10-31 12:10:26 +0700

    membuat homepage dan itempage

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
+import 'package:belanja/pages/home_page.dart';
+import 'package:belanja/pages/item_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Chapter 5',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Text('Shopping List'),
-    );
-  }
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/': (context) => HomePage(),
+      '/item': (context) => ItemPage(),
+    },
+  ));
 }
