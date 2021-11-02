94b74c211436f46eb4767cb5c730d68e14088442
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-11-02 11:55:30 +0700

    Praktikum 1 - Mendefinisikan Route

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,19 @@
+import 'package:belanja/models/Item.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+    );
   }
 }
